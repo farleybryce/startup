7af22e882ff5e28d30c9d44fd448315283263400
Bryce Farley <brycesfarley@gmail.com>
2026-02-11 16:51:10 -0700

    header and footer template added

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="body bg-dark text-light">
+    <header>
+      <nav className="navbar fixed-top">
+        <menu>
+          <li className="nav-item"><a href="index.html">Home</a></li>
+          <li className="vert_bar">|</li>
+          <li className="nav-item"><a href="game.html">Daily Game</a></li>
+          <li className="vert_bar">|</li>
+          <li className="nav-item"><a href="leaderboard.html">Leaderboard</a></li>
+          <li className="vert_bar">|</li>
+          <li className="nav-item"><a href="how_to_play.html">How to Play</a></li>
+        </menu>
+      </nav>
+    </header>
+    
+
+    <main>App components go here</main>
+
+    <footer>
+      <div className="footer_content">Logged in as: [user]</div>
+      <div className="footer_content">Author: Bryce Farley</div>
+      <a  className="footer_content" href="https://github.com/farleybryce/startup.git">GitHub</a>
+    </footer>
+  </div>;
 }
