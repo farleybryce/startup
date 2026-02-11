f2b86ebd813947d94533e47c62ced84357df073e
Bryce Farley <brycesfarley@gmail.com>
2026-02-11 16:55:23 -0700

    individual jsx page components created

---
+++ src/how_to_play/how_to_play.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function How_To_Play() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>how to play displayed here</div>
+    </main>
+  );
+}
