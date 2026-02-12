22b4644054b8985e2d7d40f3673ce4568004e10f
Bryce Farley <brycesfarley@gmail.com>
2026-02-11 17:08:25 -0700

    routing added

--- src/app.jsx
+++ src/app.jsx
@@ -2,29 +2,49 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Game } from './game/game';
+import { Leaderboard } from './leaderboard/leaderboard';
+import { How_To_Play } from './how_to_play/how_to_play';
+
 export default function App() {
-  return <div className="body bg-dark text-light">
-    <header>
-      <nav className="navbar fixed-top">
-        <menu>
-          <li className="nav-item"><a href="index.html">Home</a></li>
-          <li className="vert_bar">|</li>
-          <li className="nav-item"><a href="game.html">Daily Game</a></li>
-          <li className="vert_bar">|</li>
-          <li className="nav-item"><a href="leaderboard.html">Leaderboard</a></li>
-          <li className="vert_bar">|</li>
-          <li className="nav-item"><a href="how_to_play.html">How to Play</a></li>
-        </menu>
-      </nav>
-    </header>
-    
+  return (
+  <BrowserRouter>
+    <div className="body">
+      <header>
+        <nav className="navbar fixed-top">
+          <menu>
+            <li className="nav-item"><NavLink to="/">Home</NavLink></li>
+            <li className="vert_bar">|</li>
+            <li className="nav-item"><NavLink to="game">Daily Game</NavLink></li>
+            <li className="vert_bar">|</li>
+            <li className="nav-item"><NavLink to="leaderboard">Leaderboard</NavLink></li>
+            <li className="vert_bar">|</li>
+            <li className="nav-item"><NavLink to="how_to_play">How to Play</NavLink></li>
+          </menu>
+        </nav>
+      </header>
+      
 
-    <main>App components go here</main>
+      <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/game' element={<Game />} />
+        <Route path='/leaderboard' element={<Leaderboard />} />
+        <Route path='/how_to_play' element={<How_To_Play />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
 
-    <footer>
-      <div className="footer_content">Logged in as: [user]</div>
-      <div className="footer_content">Author: Bryce Farley</div>
-      <a  className="footer_content" href="https://github.com/farleybryce/startup.git">GitHub</a>
-    </footer>
-  </div>;
+      <footer>
+        <div className="footer_content">Logged in as: [user]</div>
+        <div className="footer_content">Author: Bryce Farley</div>
+        <a  className="footer_content" href="https://github.com/farleybryce/startup.git">GitHub</a>
+      </footer>
+    </div>
+  </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
